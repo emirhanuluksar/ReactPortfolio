2fdea6a1bbbff478dd312eb6c084467d613f157f
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-05-27 20:59:45 +0300

    About component is created

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,52 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import "./about.css";
-
+import AboutImage from "../../assets/header.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={AboutImage} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>180+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+            tenetur repellat, asperiores maxime tempore explicabo autem aut
+            excepturi voluptatem at quibusdam ut velit, vero doloremque eius
+            assumenda optio. Alias, aliquid!
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
